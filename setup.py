bfd1e2d8cc52e3196ad3dfef8fac3eb0bba41f3a
Doruk Onur Caliskan <63496036+dorukcaliskan02@users.noreply.github.com>
2024-04-17 19:19:40 +0300

    name added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     # TODO: Write a globally unique name which will be listed on PyPI
     name="very-simple-dictionary",
-    author="Author Name",  # TODO: Write your name
+    author="Author Name",  # TODO: Doruk Onur Caliskan
     version="2.0.0",
     packages=["dictionary"],
     install_requires=[
